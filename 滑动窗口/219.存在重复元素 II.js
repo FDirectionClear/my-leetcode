00e4952aa09521cb0c64a7615d8ffc4498907d2f
F·Director <fangxiangming@xiaomi.com>
2023-03-06 10:44:23 +0800

    Update 219.存在重复元素 II.js

--- 滑动窗口/219.存在重复元素 II.js
+++ 滑动窗口/219.存在重复元素 II.js
@@ -1,3 +1,23 @@
+/**
+ * 我的答案：滑动窗口法
+ * 和题解答案基本类似。
+ * 滑动窗口法，有些时候不需要双重循环，不要基于双重循环的思维定式去入手
+ */
 var containsNearbyDuplicate = function(nums, k) {
-  
+  let set = new Set()
+  let j = 0
+
+  for (let i = 0, len = nums.length; i < len; i ++) {
+    if (i - j > k) {
+      set.delete(nums[j])
+      j ++
+    }
+    if (!set.has(nums[i])) {
+      set.add(nums[i])
+    } else {
+      return true
+    }
+  }
+
+  return false
 };
