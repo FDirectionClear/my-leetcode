78e984d2117e52ce7f8668d1583c9d1424bef0c5
F·Director <fangxiangming@xiaomi.com>
2025-07-30 15:09:10 +0800

    feat: 重做198打家劫舍

--- playground.js
+++ playground.js
@@ -1,47 +1,18 @@
-// 截止到i（包含i）
-max[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]); // 不取当前数，之前的最大值，可能是前一个取或者不取的情况
-max[i][1] = Math.max(nums[i], dp[i - 1][1] * nums[i]); // 必须取当前数，分为两种情况：必须看取前一个数的最大值和当前数的乘机。取前一个数的最大值和当前数构成的值更小了（比如dp[i-1][1]是-2，nums[i]是1，则不如从nums[i]单独开一个连续的子数组）
+dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+dp[i][1] = dp[i - 1][0] + nums[i];
 
 /**
- * 这道题的动态转移方程非常多种情况。因为nums[i]可以是正负数，而最大值和nums[i]的符号强相关。
- * 因此我们要加入分类讨论。
- *
- * 考虑的是通过三维的dp来储存状态，但是考虑到三维的dp维护起来太复杂（通过第三维来区分最小值和最大值），
- * 而事实上，动态转移方程的储存结果没有人能限制只能有一个变量。所以就用max和min来维护了。
+ * @param {number[]} nums
+ * @return {number}
  */
-
-var maxProduct = function (nums) {
-  const max = Array.from({ length: nums.length }, () =>
-    new Array(2).fill(-Infinity)
-  );
-  const min = Array.from({ length: nums.length }, () =>
-    new Array(2).fill(Infinity)
-  );
-
-  max[0][1] = nums[0];
-  min[0][1] = nums[0];
+var rob = function (nums) {
+  const dp = [[0, nums[0]]];
 
   for (let i = 1, len = nums.length; i < len; i++) {
-    // max[i][0]：截止到nums[i]（包含），但不取nums[i]的子数组乘积最大值
-    // max[i][1]：截止到nums[i]（包含），但必须取nums[i]的子数组乘积最大值
-    // min[i][0]：截止到nums[i]（包含），但不取nums[i]的子数组乘积最小值
-    // min[i][1]：截止到nums[i]（包含），但必须取nums[i]的子数组乘积最小值
-    max[i][0] = Math.max(max[i - 1][0], max[i - 1][1]);
-    min[i][0] = Math.min(min[i - 1][1], min[i - 1][0]);
-
-    // 因为nums[i]可以是正负数，而最大值和nums[i]的符号强相关，所以需要对正负数以及0的情况进行分类讨论
-    if (nums[i] < 0) {
-      // 如果当前数是负数
-      max[i][1] = Math.max(min[i - 1][1] * nums[i], nums[i]); // 因为需要保持连续，必须取之前连续的最大值，但结果可能是更小了，所以要和自身对比一下。
-      min[i][1] = Math.min(max[i - 1][1] * nums[i], nums[i]); // 因为需要保持连续，必须取之前连续的最大值，但结果可能是更大了，所以要和自身对比一下。
-    } else if (nums[i] > 0) {
-      max[i][1] = Math.max(max[i - 1][1] * nums[i], nums[i]); // 道理同上
-      min[i][1] = Math.min(min[i - 1][1] * nums[i], nums[i]);
-    } else {
-      max[i][1] = 0;
-      min[i][1] = 0;
-    }
+    dp[i] = [];
+    dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+    dp[i][1] = dp[i - 1][0] + nums[i];
   }
 
-  return Math.max(max[nums.length - 1][1], max[nums.length - 1][0]);
+  return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
 };
--- 动态规划与贪心/198.打家劫舍.js
+++ 动态规划与贪心/198.打家劫舍.js
@@ -1,13 +1,27 @@
+/**每一家都可以偷或者不偷，如果决定偷第i家，那么他的最大收益取决于前面那家偷或者不偷的所能实现的最大收益 */
+const rob = function (nums) {
+  const dp = [[0, nums[0]]];
+
+  for (let i = 1, len = nums.length; i < len; i++) {
+    dp[i] = [];
+    dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]); // 不偷第i家，那么前一家可以偷也可以不偷，就看前一家偷或者不偷实现的最大收益谁更大。
+    dp[i][1] = dp[i - 1][0] + nums[i]; // 一定偷第i家，那前一家一定不能偷，所以只看不偷前一家的最大收益+当前家所拥有的钱即可
+  }
+
+  return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
+};
+
+// 实际上这道题的动态转移方程也可以只有一个维度
 //dp[i]表示0-i能偷的最大金额
-const rob = (nums) => {
-    const len = nums.length;
-    const dp = [nums[0], Math.max(nums[0], nums[1])]; //初始化dp数组的前两项
-    for (let i = 2; i < len; i++) {
-        //从第三个位置开始遍历
-        //dp[i - 2] + nums[i] 表示偷当前位置，那么i-1的位置不能偷，
-      	//而且需要加上dp[i-2],也就是前i-2个房间的金钱
-        //dp[i - 1]表示偷当前位置，只偷i-1的房间
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
-    }
-    return dp[len - 1]; //返回最后最大的项
+const rob2 = (nums) => {
+  const len = nums.length;
+  const dp = [nums[0], Math.max(nums[0], nums[1])]; //初始化dp数组的前两项
+  for (let i = 2; i < len; i++) {
+    //从第三个位置开始遍历
+    //dp[i - 2] + nums[i] 表示偷当前位置，那么i-1的位置不能偷，
+    //而且需要加上dp[i-2],也就是前i-2个房间的金钱
+    //dp[i - 1]表示偷当前位置，只偷i-1的房间
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[len - 1]; //返回最后最大的项
 };
