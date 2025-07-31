b003c1a676ebf09b95e14f53369a536e4656de18
F·Director <fangxiangming@xiaomi.com>
2025-07-31 19:42:39 +0800

    feat: 重做17. 电话号码组合

--- playground.js
+++ playground.js
@@ -1,21 +1,72 @@
-var combine = function (n, k) {
-  // 回溯法
-  let result = [],
-    path = [];
-  let backtracking = (_n, _k, startIndex) => {
-    // 终止条件
-    if (path.length === _k) {
-      result.push(path.slice()); // copy一份，放入result
-      return;
+// [a]
+
+// [abc, def, ghi]
+
+// result = [abc]
+// [a] [def]
+//     [ad][ef]
+//     [ae][f]
+//     [af][]
+// [b] [def]
+//     [bd][ef]
+//     [be][f]
+//     [bf][]
+// [c] [def]
+
+// ["ad","ae","af","bd","be","bf","cd","ce","cf"] [ghi]
+
+// while
+
+// [ad] [ghi]
+//     [adg][hi]
+//     [adh][i]
+//     [adi][]
+// [ae] [ghi]
+//     ....
+
+// const results = []
+// const waitingCombine = []
+// const path = []
+
+var letterCombinations = function (digits) {
+  const letterMap = [
+    "", // 0
+    "", // 1
+    "abc", // 2
+    "def", // 3
+    "ghi", // 4
+    "jkl", // 5
+    "mno", // 6
+    "pqrs", // 7
+    "tuv", // 8
+    "wxyz", // 9
+  ];
+  let results = [],
+    waitingCombine = digits.split("").map((dig) => letterMap[dig]); // 初始化的所有点的映射
+
+  const backtracking = (target) => {
+    let tempResults = [];
+
+    if (results.length === 0) {
+      tempResults = target.split("");
+    } else {
+      for (let i = 0, len = results.length; i < len; i++) {
+        for (let j = 0, l = target.length; j < l; j++) {
+          tempResults.push(results[i] + target[j]);
+        }
+      }
     }
-    // 循环本层集合元素
-    for (let i = startIndex; i <= _n; i++) {
-      // 这道题没什么需要对根节点和当前子节点特殊处理的逻辑
-      path.push(i); // 形成下一层backtracking的根节点
-      backtracking(_n, _k, i + 1); // 递归
-      path.pop(); // 将路径回溯成根节点的状态，方便下一轮for循环形成新的根节点
-    }
+
+    results = tempResults;
   };
-  backtracking(n, k, 1);
-  return result;
+
+  while (waitingCombine.length) {
+    const target = waitingCombine.shift();
+    backtracking(target); // 拿path中的每一个根节点去和currRight中的每一个字母做组合
+  }
+  return results;
 };
+
+const digits = "23";
+
+console.log(letterCombinations(digits));
--- 回溯/17.电话号码的字母组合.js
+++ 回溯/17.电话号码的字母组合.js
@@ -23,76 +23,116 @@
 /**
  * 我的答案
  */
-const letterCombinations = function(digits) {
-  if (!digits) return []
-  const keyMap = ['', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-  const keys = digits.split('').map(item => keyMap[item - 1])
-  const result = []
-  
-  const backtrack = (prevMembers, index /**先前组合好的成员们，将要组合的是哪个键的映射成员 */) => {
-    const key = keys[index]
-    for (let i = 0, len = key.length; i < len; i ++) {
+const letterCombinations = function (digits) {
+  if (!digits) return [];
+  const keyMap = ["", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+  const keys = digits.split("").map((item) => keyMap[item - 1]);
+  const result = [];
+
+  const backtrack = (
+    prevMembers,
+    index /**先前组合好的成员们，将要组合的是哪个键的映射成员 */
+  ) => {
+    const key = keys[index];
+    for (let i = 0, len = key.length; i < len; i++) {
       if (index === keys.length - 1) {
-        result.push(prevMembers + key[i])
+        result.push(prevMembers + key[i]);
       } else {
-        backtrack(prevMembers + key[i], index + 1)
+        backtrack(prevMembers + key[i], index + 1);
       }
     }
+  };
+
+  backtrack("", 0);
+
+  return result;
+};
+
+/**
+ * 还有一种的我的答案
+ */
+var letterCombinations2 = function (digits) {
+  const letterMap = [
+    "", // 0
+    "", // 1
+    "abc", // 2
+    "def", // 3
+    "ghi", // 4
+    "jkl", // 5
+    "mno", // 6
+    "pqrs", // 7
+    "tuv", // 8
+    "wxyz", // 9
+  ];
+  let results = [],
+    waitingCombine = digits.split("").map((dig) => letterMap[dig]); // 初始化的所有点的映射
+
+  const backtracking = (target) => {
+    let tempResults = [];
+
+    if (results.length === 0) {
+      tempResults = target.split("");
+    } else {
+      for (let i = 0, len = results.length; i < len; i++) {
+        for (let j = 0, l = target.length; j < l; j++) {
+          tempResults.push(results[i] + target[j]);
+        }
+      }
+    }
+
+    results = tempResults;
+  };
+
+  while (waitingCombine.length) {
+    const target = waitingCombine.shift();
+    backtracking(target); // 拿path中的每一个根节点去和currRight中的每一个字母做组合
   }
-
-  backtrack('', 0)
-
-  return result
+  return results;
 };
 
 /**
  * 题解答案：dfs+回溯
  */
-var letterCombinations2 = (digits) => {
+var letterCombinations3 = (digits) => {
   if (digits.length == 0) return [];
   const res = [];
-  const map = {//建立电话号码和字母的映射关系
-      2: "abc",
-      3: "def",
-      4: "ghi",
-      5: "jkl",
-      6: "mno",
-      7: "pqrs",
-      8: "tuv",
-      9: "wxyz",
+  const map = {
+    //建立电话号码和字母的映射关系
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
   };
 
-  const dfs = (curStr, i) => {//curStr是递归每一层的字符串，i是扫描的指针
-      if (i > digits.length - 1) {//边界条件，递归的出口
-          res.push(curStr); //其中一个分支的解推入res
-          return; //结束递归分支，进入另一个分支
-      }
-      const letters = map[digits[i]]; //取出数字对应的字母
-      for (const l of letters) {
-          //进入不同字母的分支
-          dfs(curStr + l, i + 1); //参数传入新的字符串，i右移，继续递归
-      }
+  const dfs = (curStr, i) => {
+    //curStr是递归每一层的字符串，i是扫描的指针
+    if (i > digits.length - 1) {
+      //边界条件，递归的出口
+      res.push(curStr); //其中一个分支的解推入res
+      return; //结束递归分支，进入另一个分支
+    }
+    const letters = map[digits[i]]; //取出数字对应的字母
+    for (const l of letters) {
+      //进入不同字母的分支
+      dfs(curStr + l, i + 1); //参数传入新的字符串，i右移，继续递归
+    }
   };
   dfs("", 0); // 递归入口，传入空字符串，i初始为0的位置
   return res;
 };
 
-var digits = "23"
-console.log(
-  letterCombinations(digits)
-)
+var digits = "23";
+console.log(letterCombinations(digits));
 
-var digits = ""
-console.log(
-  letterCombinations(digits)
-)
+var digits = "";
+console.log(letterCombinations(digits));
 
-var digits = "2"
-console.log(
-  letterCombinations(digits)
-)
+var digits = "2";
+console.log(letterCombinations(digits));
 
-var digits = "22"
-console.log(
-  letterCombinations(digits)
-)
+var digits = "22";
+console.log(letterCombinations(digits));
