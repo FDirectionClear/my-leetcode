e403cbd07a26b36f453ed115c4b1f9ccba174cbf
芳苘眀 <35694038+FDirectionClear@users.noreply.github.com>
2025-08-14 10:19:44 +0800

    Update 496. 下一个更大元素 I.js

--- 单调栈/496. 下一个更大元素 I.js
+++ 单调栈/496. 下一个更大元素 I.js
@@ -84,3 +84,25 @@ var nums1 = [4, 1, 2],
 //   nums2 = [1, 2, 3, 4];
 
 console.log(nextGreaterElement(nums1, nums2));
+
+/**
+ * 代码随想录的答案，实际上和我的想法如出一辙
+ */
+var nextGreaterElement = function (nums1, nums2) {
+  let stack = [];
+  let map = new Map();
+  for (let i = 0; i < nums2.length; i++) {
+    while (stack.length && nums2[i] > nums2[stack[stack.length - 1]]) {
+      let index = stack.pop();
+      map.set(nums2[index], nums2[i]);
+    }
+    stack.push(i);
+  }
+
+  let res = [];
+  for (let j = 0; j < nums1.length; j++) {
+    res[j] = map.get(nums1[j]) || -1;
+  }
+
+  return res;
+};
