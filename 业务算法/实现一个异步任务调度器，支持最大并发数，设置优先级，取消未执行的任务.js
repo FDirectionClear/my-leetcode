c61af2dddbbf7732157d279084f587e2cf3d00c8
F·Director <fangxiangming@xiaomi.com>
2026-01-06 17:23:35 +0800

    业务算法

--- 业务算法/实现一个异步任务调度器，支持最大并发数，设置优先级，取消未执行的任务.js
+++ 业务算法/实现一个异步任务调度器，支持最大并发数，设置优先级，取消未执行的任务.js
@@ -8,7 +8,9 @@ class AsyncTaskScheduler {
    * @param {number} maxConcurrent - 最大并发任务数，默认值：3
    */
   constructor(maxConcurrent = 3) {
-    
+    this.taskQueue = []; // 待执行的任务队列，config队列
+    this.currentCount = 0; // 当前正在并发的任务数
+    this.maxConcurrent = maxConcurrent;
   }
 
   /**
@@ -17,24 +19,115 @@ class AsyncTaskScheduler {
    * @param {number} options.priority - 任务优先级（数字越大优先级越高），默认值：0
    * @param {string} options.id - 任务唯一标识
    */
-  addTask(taskFn, options = {}) {}
+  addTask(taskFn, options = {}) {
+    const { id, priority = 0 } = options;
+    const taskConfig = {
+      id,
+      taskFn,
+      priority,
+    };
+    this.taskQueue.push(taskConfig);
+    this.taskQueue.sort((a, b) => b.priority - a.priority); // 根据优先级从大到小排列
+    console.log("排序结果", [...this.taskQueue]);
+    this._scheduler();
+  }
 
   /**
    * 取消指定任务
    * @param {string} taskId - 任务ID
    * @returns {boolean} 是否取消成功
    */
-  cancelTask(taskId) {}
+  cancelTask(taskId) {
+    try {
+      let index = this.taskQueue.findIndex(
+        (taskConfig) => taskConfig.id === taskId
+      );
+
+      if (index !== -1) {
+        this.taskQueue.splice(index, 1); // 删除当前id对应的任务config
+        return true;
+      }
+    } catch {
+      // 如果逻辑执行出错也返回false
+      return false;
+    }
+
+    // 删除不存在的任务也是false
+    return false;
+  }
 
   /**
    * 获取调度器状态
    * @returns {Object} 状态对象
    */
-  getStatus() {}
+  getStatus() {
+    return {
+      currentCount: this.currentCount, // 返回当前正在并发的数量
+      tasks: this.taskQueue, // 当前还未执行的任务
+    };
+  }
 
   /**
    * 清空所有等待中的任务
    * @returns {number} 被取消的任务数量
    */
-  clearPendingTasks() {}
+  clearPendingTasks() {
+    const cancelCount = this.taskQueue.length;
+    this.taskQueue = [];
+
+    return cancelCount;
+  }
+
+  /**
+   * 内置的任务调度器
+   */
+  _scheduler() {
+    if (this.currentCount >= this.maxConcurrent) return true;
+    // 如果当前并发的任务数 < 最大并发数限制
+    const { taskFn } = this.taskQueue.shift() ?? {};
+
+    if (typeof taskFn === "function") {
+      this.currentCount++;
+      taskFn().then((res) => {
+        this.currentCount--;
+        // 递归进行并发
+        this._scheduler();
+      });
+    }
+  }
 }
+
+const mockAsyncTask = (id, duration = 1000) => {
+  return () =>
+    new Promise((resolve) => {
+      console.log(`${id}.开始`);
+      setTimeout(() => {
+        console.log(`${id}.完成，尝试进行下一个`);
+        resolve(`${id}.完成，尝试进行下一个`);
+      }, duration);
+    });
+};
+
+const queue = Array.from({ length: 10 }, (_, index) =>
+  mockAsyncTask(index + 1)
+);
+
+const asyncTaskScheduler = new AsyncTaskScheduler(3);
+
+// 上来先尝试并发10个
+queue.forEach((taskFn, index) => {
+  asyncTaskScheduler.addTask(taskFn, { id: index + 1, priority: 1 });
+});
+
+// 1s后删除某个任务
+setTimeout(() => {
+  console.log("删除1任务10");
+  asyncTaskScheduler.cancelTask(10);
+}, 2000);
+
+// 2s添加两个
+setTimeout(() => {
+  console.log("添加两个任务");
+  asyncTaskScheduler.addTask(mockAsyncTask(11), { id: 11, priority: 1 });
+  asyncTaskScheduler.addTask(mockAsyncTask(12), { id: 12, priority: 10 });
+}, 2000);
---
+++ 业务算法/实现按请求顺序打印请求结果.js
@@ -0,0 +1,71 @@
+/**
+ * from: 美团1面
+ * 题目：假设有10个ajax请求，每个请求返回的时间必然是不固定的，但是请求的顺序是固定的
+ * 要求：先请求的结果先打印，即使后请求的比先请求的提前返回，也要等先请求的打印完成后才能打印。
+ */
+
+function mockPost(url, res, duration = Math.random() * 1500) {
+  return new Promise((resolve) => {
+    console.log(`${url}请求开始 =>`);
+    setTimeout(() => {
+      console.log(`${url}请求返回 <=`, res);
+      resolve(res);
+    }, duration);
+  });
+}
+
+class ResByOrderReqeust {
+  constructor() {
+    this.resQueue = []; // 响应的config
+    this.waitingOrder = 1; // 等待的order
+    this.requestOrder = 0; // 最新请求的序号
+  }
+
+  request(url, duration) {
+    const requestConfig = {
+      url,
+      order: ++this.requestOrder,
+    };
+
+    mockPost(url, requestConfig, duration).then(this.scheduleRes.bind(this));
+  }
+
+  // 用于调度响应情况
+  scheduleRes(res) {
+    const { order } = res;
+
+    if (order === this.waitingOrder) {
+      // 如果当前返回的order是正在等待的Order，那么直接打印
+      console.log(`第${order}个请求打印`, res);
+      this.findRes(order + 1); // 在已经返回的请求中找
+    } else {
+      this.resQueue.push(res); // 如果不是就直接推入队列等待
+    }
+  }
+
+  findRes(targetOrder) {
+    const targetIndex = this.resQueue.findIndex(
+      (item) => item.order === targetOrder
+    ); // 找到当前目标请求的索引
+
+    if (targetIndex !== -1) {
+      // 如果说能找到
+      console.log(
+        `第${this.resQueue[targetIndex].order}个请求打印`,
+        this.resQueue[targetIndex]
+      );
+      this.findRes(targetOrder + 1); // 递归找下一个
+    } else {
+      // 如果找不到，说明目标还没返回，登记该目标，等下次出现该目标就直接打印
+      this.waitingOrder++;
+    }
+  }
+}
+
+const requestInstance = new ResByOrderReqeust();
+
+requestInstance.request("url1");
+requestInstance.request("url2");
+requestInstance.request("url3");
+requestInstance.request("url4");
+requestInstance.request("url5");
